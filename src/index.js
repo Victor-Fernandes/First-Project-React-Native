2ab0f72bb08a3ef0358270c3792b3a0d239d68a2
Victor Fernandes <victor.fernandes.07@gmail.com>
2019-10-03 14:15:45 -0300

    começando a navigation

--- src/index.js
+++ src/index.js
@@ -1,26 +1,9 @@
 import React from 'react';
-import {StyleSheet, View, Text} from 'react-native';
+import {View} from 'react-native';
 
 import './config/ReactotronConfig';
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCCF',
-  },
-  welcome: {
-    fontSize: 30,
-    alignItems: 'center',
-    margin: 10,
-  },
-});
+import Routes from './routes';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text styles={styles.welcome}>welcome to react-native</Text>
-    </View>
-  );
+  return <Routes />;
 }
---
+++ src/routes.js
@@ -0,0 +1,18 @@
+import {createAppContainer} from 'react-navigation';
+import {createStackNavigator} from 'react-navigation-stack';
+
+import Main from './pages/Main';
+import User from './pages/User';
+
+const Routes = createAppContainer(
+  // createAppContainer => configuração das rotas, sempre precisa vir por volta
+  // de todas as rotas
+  createStackNavigator({
+    // Contem 1 tipo d configuracao d rotas, cada vez q o usuario acessa uma rota
+    // gera uma pilha e deixa a rota em background.
+    Main,
+    User,
+  })
+);
+
+export default Routes;
